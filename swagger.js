d4abd85cbe41620dd0a65721a3efb07d8841b8ea
GregoireEpsi <gregoire.barre@epsi.fr>
2021-03-10 13:13:40 +0100

    add doc global

--- swagger.js
+++ swagger.js
@@ -3,7 +3,25 @@ const swaggerAutogen = require('swagger-autogen')()
 const outputFile = './swagger_output.json'
 const endpointsFiles = ['./server.js']
 
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
+
+const doc = {
+    info: {
+        version: "1.0.0",
+        title: "API SignIn/SignUp",
+        description: ""
+    },
+    host: "localhost:8080",
+    basePath: "/",
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+
+
+}
+
+
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
     require('./server.js')
-    
+
 })
